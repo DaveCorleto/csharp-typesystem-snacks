9ef161c16312e04b7527515f8367c58325cd281b
DaveCorleto <davide.corleto@fastwebnet.it>
2024-04-17 15:22:17 +0200

    Snack3

--- Program.cs
+++ Program.cs
@@ -41,59 +41,84 @@ namespace csharp_typesystem_snacks
             //Snacks 2 
 
 
-            Console.WriteLine("Inserisci la prima parola");
+            //Console.WriteLine("Inserisci la prima parola");
 
-            string word1 = Console.ReadLine();
+            //string word1 = Console.ReadLine();
 
-            Console.WriteLine("Inserisci la seconda parola");
+            //Console.WriteLine("Inserisci la seconda parola");
 
-            string word2 = Console.ReadLine();
+            //string word2 = Console.ReadLine();
 
-            string straightword = "";
-            string longword = "";
+            //string straightword = "";
+            //string longword = "";
 
-            if (word1.Length > word2.Length)
-            {
-                straightword = word2;
-                longword = word1;
-            }
-            else if (word2.Length > word1.Length)
-            {
-                longword = word2;
-                straightword = word1;
-            }
-            else
-            {
-                Console.WriteLine("Le parole che hai inserito hanno la stessa lunghezza!");
-            }
+            //if (word1.Length > word2.Length)
+            //{
+            //    straightword = word2;
+            //    longword = word1;
+            //}
+            //else if (word2.Length > word1.Length)
+            //{
+            //    longword = word2;
+            //    straightword = word1;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Le parole che hai inserito hanno la stessa lunghezza!");
+            //}
 
-            Console.WriteLine($"Hai inserito le parole: {straightword} e {longword} ");
+            //Console.WriteLine($"Hai inserito le parole: {straightword} e {longword} ");
+
+
+            //Snack3 
+
+            //    Console.WriteLine("Inserisci un numero: ");
+
+            //    int[] tenNumbers = new int[10];
+            //    int index = 0;
+
+            //    foreach (var number in tenNumbers)
+            //    {
+            //        Console.WriteLine("Inserisci un numero: ");
+            //        tenNumbers[index] = int.Parse(Console.ReadLine());
+            //        index++;
+            //    }
+
+            //    int somma = 0;
+
+            //    foreach (var number in tenNumbers)
+            //    {
+            //        somma += number;
+            //    }
+
+            //    Console.WriteLine($"La somma dei numeri che hai inserito è: {somma} ");
+
+            //}
+
+            //Snack 4
+            //Calcola la somma e la media dei numeri da 2 a 10.
+
+            
 
         }
+
+
+
+        //Snack 4
+        //Calcola la somma e la media dei numeri da 2 a 10.
+        //Snack 5
+        //Il software chiede all’utente di inserire un numero.Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
+        //Snack 6
+        //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
+        //    Snack 7
+        //Crea un array vuoto.
+        //Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+        //    Snack 8
+        //Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+        //Snack 9
+        //Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.Continua a chiedere i numeri all’utente e a inserirli nell’array, fino a quando la somma degli elementi è minore di 50.
+        //Snack 10
+        //Fai inserire un numero, che chiameremo N, all’utente.
+        //Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+        //Ogni volta che ne crei uno, stampalo a schermo.
     }
-
-
-    //    Snack 2
-    //L’utente inserisce due parole in successione.
-    //    Il software stampa prima la parola più corta, poi la parola più lunga.
-    //Snack 3
-    //Il software deve chiedere per 10 volte all’utente di inserire un numero.
-    //    Il programma stampa la somma di tutti i numeri inseriti.
-    //Snack 4
-    //Calcola la somma e la media dei numeri da 2 a 10.
-    //Snack 5
-    //Il software chiede all’utente di inserire un numero.Se il numero inserito è pari, stampa il numero, se è dispari, stampa il numero successivo.
-    //Snack 6
-    //In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby. Chiedi all’utente il suo nome e comunicagli se può partecipare o meno alla festa.
-    //    Snack 7
-    //Crea un array vuoto.
-    //Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
-    //    Snack 8
-    //Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
-    //Snack 9
-    //Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.Continua a chiedere i numeri all’utente e a inserirli nell’array, fino a quando la somma degli elementi è minore di 50.
-    //Snack 10
-    //Fai inserire un numero, che chiameremo N, all’utente.
-    //Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
-    //Ogni volta che ne crei uno, stampalo a schermo.
-}
